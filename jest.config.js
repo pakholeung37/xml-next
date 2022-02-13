9d98a5087094535f1c65d73db5476526c8f97428
Pakho Leung <pakholeung37@gmail.com>
2022-02-14 00:02:45 +0800

    build: fix jest eslint

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
+const rootDir = __dirname
 module.exports = {
   testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: ['packages/**/*.{ts,tsx}'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  modulePathIgnorePatterns: [
-    '<rootDir>/examples',
-    '<rootDir>/tooling/cra-template*',
-  ],
+  modulePathIgnorePatterns: [`examples`, 'tooling'],
   transform: {
+    '\\.xml$': `${rootDir}/scripts/jest-xml.js`,
     '^.+\\.(ts|tsx|js|jsx)?$': '@swc-node/jest',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
